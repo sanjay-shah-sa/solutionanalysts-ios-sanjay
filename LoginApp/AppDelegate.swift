fc6482492cba4fede86cd67951be26c53645ee81
sanjaybhansali108 <sanjaybhansali108@gmail.com>
2021-01-03 10:26:37 +0530

    [config] initiate project

---
+++ LoginApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  LoginApp
+//
+//  Created by Sanjay Shah on 03/01/2021.
+//  Copyright © 2021 Sanjay Shah. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
